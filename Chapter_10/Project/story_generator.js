2be85b6c63c6f717eba2b67ca29f64118f2c5633
BalaurZmeu <balaur.zmeu@gmail.com>
2023-12-14 21:03:11 +0200

    Finished the Project for Chapter 10

--- Chapter_10/Project/story_generator.js
+++ Chapter_10/Project/story_generator.js
@@ -4,33 +4,44 @@
 *
 * This file contains functions that support the story_generator.html
 *******************************************************************/
+// Global variables accessible for all functions
 
+var inputForm;  // User entered words
+var resultPage; // Part of the page with text of the story
+var inputPage;  // Part of the page with the input form
+
+/******************************************************************/
 // This function replaces the missing text with the words from
 // the user input form, hides the form, and shows the result
 
 function generateText(form) {
-  var formValues; // Collection of user entered words
-
-  formValues = form.elements[]
-  for (let element of formValues) {
-    
+  inputForm = document.getElementById("input-form");
+  resultPage = document.getElementById("result-page");
+  inputPage = document.getElementById("input-page");
+  
+  for (let i=0; i<5; i++) {
+    resultPage.children[i].innerHTML =
+      inputForm.children[i].children[1].value;    
   }
-
-}
+  
+  resultPage.style.display = "block";
+  inputPage.style.display = "none";
+} // end generateText
 
 /******************************************************************/
-
 // This function clears the user input form
 
-function reset(form) {
-
+function resetForm() {
+  for (let i=0; i<5; i++) {
+    inputForm.children[i].children[1].value = "";
+  }
 }
 
 /******************************************************************/
-
 // This function hides the result section with the story and
 // shows the user input form
 
 function goBack() {
-
+  inputPage.style.display = "block";
+  resultPage.style.display = "none";
 }
