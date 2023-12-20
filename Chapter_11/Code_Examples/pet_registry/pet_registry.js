a93a0c1a9ca7a604f1090b943dcb2d3baf81e7a8
BalaurZmeu <balaur.zmeu@gmail.com>
2023-12-20 13:50:53 +0200

    Finished the pet_registry Code Example for Chapter 11

--- Chapter_11/Code_Examples/pet_registry/pet_registry.js
+++ Chapter_11/Code_Examples/pet_registry/pet_registry.js
@@ -6,7 +6,7 @@
 *******************************************************************/
 
 class Pet {
-  constructor() {
+  constructor(name, sound) {
     this.name = name;
     this.sound = sound;
   } // end constructor
@@ -43,6 +43,47 @@ class Dog extends Pet {
 
 class Hedgehog extends Pet {
   constructor(name) {
-    super(name, "snuffle, click, purr");
+    super(name, "snuffle, click, purr.");
   } // end constructor
 } // end class Hedgehog
+
+//******************************************************************
+
+// This function reveals or hides supplemental input controls.
+
+function adjustForm(form) {
+  if (form.elements["petType"].value == "dog") {
+    document.getElementById("dog-more-info").style.display =
+      "table-cell";
+  }
+  else {
+    document.getElementById("dog-more-info").style.display = "none";
+  }
+} // end adjustForm
+
+//******************************************************************
+
+// This function registers a pet.
+
+function register(form) {
+  var name;   // pet's name
+  var trick;  // dog's favorite trick
+  var pet;    // an object that's a subclass of Pet
+
+  if (!form.checkValidity()) {
+    document.getElementById("message").innerHTML =
+      "You must enter values for all fields";
+  }
+  else {
+    name = form.elements["name"].value;
+    if (form.elements["petType"].value == "dog") {
+      trick = form.elements["trick"].value;
+      pet = new Dog(name, trick);
+    }
+    else {
+      pet = new Hedgehog(name);
+    }
+    
+    document.getElementById("message").innerHTML = pet.confirm();
+  } // end else of (!form.checkValidity())
+} // end register
