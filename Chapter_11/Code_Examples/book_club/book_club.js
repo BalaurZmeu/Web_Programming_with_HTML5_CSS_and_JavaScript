2fedc6d2190383def40242dab82d8d3609bbbd66
BalaurZmeu <balaur.zmeu@gmail.com>
2023-12-21 21:51:07 +0200

    Finished the book_club Code Example for Chapter 11

--- Chapter_11/Code_Examples/book_club/book_club.js
+++ Chapter_11/Code_Examples/book_club/book_club.js
@@ -25,3 +25,47 @@ class BookMeeting {
       "</cite></span></div>";
   } // end bookMtgEntry
 } // end class BookMeeting
+
+//******************************************************************
+
+var bookMtgList = new Array();
+
+// Add a book club meeting to the list.
+
+function addMtg(form) {
+  var author;  // book author
+  var title;   // book title
+  var date;    // book club meeting date
+  
+  if (!form.checkValidity()) {
+    document.getElementById("error").style.display = "block";
+  }
+  else {
+    document.getElementById("error").style.display = "none";
+    document.getElementById("mtgHeader").style.display = "block";
+    author = form.elements["author"].value;
+    title = form.elements["title"].value;
+    date = new Date(form.elements["date"].value);
+    
+    bookMtgList.push(new BookMeeting(author, title, date));
+    bookMtgList.sort(
+      function (a, b) {
+        return (a.date < b.date) ? -1 : (a.date > b.date) ? 1 : 0;
+      }
+    ); // end sort
+    displayList();
+  } // end else
+} // end addMtg
+
+//******************************************************************
+
+// Display the list of book club meetings.
+
+function displayList() {
+  var listContent = ""; // The contents of the list of book meetings
+  
+  for (let i=0; i<bookMtgList.length; i++) {
+    listContent += bookMtgList[i].bookMtgEntry();
+  }
+  document.getElementById("list").innerHTML = listContent;
+} // end displayList
