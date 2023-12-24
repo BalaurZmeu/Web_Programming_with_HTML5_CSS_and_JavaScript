af34e01f77557fce709cb79dbcafbc8fa66961a5
BalaurZmeu <balaur.zmeu@gmail.com>
2023-12-24 13:00:24 +0200

    Started to work on the Project for Chapter 11

---
+++ Chapter_11/Project/statistics.js
@@ -0,0 +1,13 @@
+/******************************************************************
+* statistics.js
+* Balaur Zmeu
+*
+* This file contains the definition of the NumberList class and
+* its methods. This file supports the statistics.html web page.
+******************************************************************/
+
+class NumberList {
+  constructor () {
+  
+  } // end constructor
+} // end class NumberList
