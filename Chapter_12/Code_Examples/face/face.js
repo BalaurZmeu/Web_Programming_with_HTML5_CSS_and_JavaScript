29d7cca9cdedda4d8cb5164e50bd58c4b257477e
BalaurZmeu <balaur.zmeu@gmail.com>
2023-12-31 08:40:28 +0200

    Working on the face Code Example for Chapter 12

---
+++ Chapter_12/Code_Examples/face/face.js
@@ -0,0 +1,12 @@
+/******************************************************************
+* face.js
+* John Dean
+*
+* This file handles drawing a face.
+******************************************************************/
+
+// This function draws the initial face.
+
+function initialize() {
+
+} // end initialize
