64dc9c2294d521a4b9a78fdd88a07363619d47e1
BalaurZmeu <balaur.zmeu@gmail.com>
2024-01-04 12:29:39 +0200

    Still working on Case Study for Chapter 12

--- Chapter_12/Case_Study/solar_angles.js
+++ Chapter_12/Case_Study/solar_angles.js
@@ -48,6 +48,8 @@ function setup(form) {
 
 /****************************************************************/
 
+// This function draws one array of panels
+
 function drawOneArray(yO) {
   var scaleFac;   // horizontal projection of sloping surface
   var width;      // image width
@@ -68,3 +70,43 @@ function drawOneArray(yO) {
   context.lineTo(XO + width, yO);
   context.stroke();
 } // end drawOneArray
+
+/****************************************************************/
+
+// This function is called when the Show Behavior button is clicked
+
+function showBehavior() {
+  var degradation;                // shadowing effect
+  var output;                     // electrical output
+  var direct;                     // direct solar
+  
+  computeAngles();
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  if (altitude > 0) {             // if daytime
+    canvas.style.backgroundColor = "lightgray";
+    showOneShadow(XO, YO);            // from north panel
+    showOneShadow(XO, YO + spacing);  // from south panel
+    drawOneArray(YO);
+    drawOneArray(YO + spacing);
+    degradation = Math.pow(getObscuration(XO, YO), 0.38);
+    degradation +=
+      Math.pow(getObscuration(XO, YO + spacing), 0.38);
+    form.elements["azimuth"].value = Math.round(azimuth *
+      180 / Math.PI);
+    form.elements["altitude"].value = Math.round(altitude *
+      180 / Math.PI);
+    direct = 0.86 * cosIncidenceAngle * (1.0 - 0.5 * degradation);
+    // 1985 ASHRAE Fundamentals Guide, Chapter 27, Diffuse Solar:
+    output = parseInt(100 * (direct + 0.14 * Math.sin(altitude)));
+  } // end if daytime
+  else {                        // nighttime
+    canvas.style.backgroundColor = "midnightblue";
+    form.elements.["azimuth"].value = "";
+    form.elements.["altitude"].value = "";
+    output = 0;
+  } // end nighttime
+  context.fillStyle = "black";
+  context.font = "1em Tahoma";
+  context.fillText("Electrical Output: " + output + " %", 10, 25);
+  context.fillText("(North)", 324, 20);
+} // end showBehavior
