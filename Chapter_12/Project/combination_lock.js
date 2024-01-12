f3aad047c50496f5d2849f9a039a63a55665ade0
BalaurZmeu <balaur.zmeu@gmail.com>
2024-01-12 14:32:01 +0200

    Finished the Project for Chapter 12

--- Chapter_12/Project/combination_lock.js
+++ Chapter_12/Project/combination_lock.js
@@ -10,10 +10,12 @@
 
 const CENTER_X = 250;
 const CENTER_Y = 200;
-
 const RADIUS = 180;
 
 var ctx;    // canvas object's context
+var number; // this variable holds the number under the red mark
+var timer;  // timer for rotating to target
+var target; // target number to rotate toward
 
 /***************************************************************/
  
@@ -24,6 +26,7 @@ function initialize() {
   ctx = document.getElementById("canvas").getContext("2d");
   drawLock();
   drawNumbers();
+  number = 0;
 } // end initialize
 
 /***************************************************************/
@@ -32,9 +35,12 @@ function initialize() {
 // background of the lock without the logo and numbers
 
 function drawLock() {
+  ctx.save();
+  ctx.resetTransform();
   drawBorder();
   drawTriangle();
   drawDial();
+  ctx.restore();
 } // end drawLock
 
 /***************************************************************/
@@ -100,33 +106,86 @@ function drawDial() {
 
 /***************************************************************/
 
-// This function draws forty marks and numbers,
-// and also the logo
+// This function draws forty white marks and numbers,
+// and also draws the logo
 
 function drawNumbers() {
   ctx.strokeStyle = "white";
   ctx.fillStyle = "white";
+  ctx.lineWidth = 4;
   ctx.textAlign = "center";
+  ctx.textBaseline = "alphabetic";
   ctx.font = "bold 22px Helvetica, sans-serif";
+    
+  // iterate 40 times
   for (let i=0; i<40; i++) {
+    
+    // draw a long mark and a number
+    // whenever the i is divisible by 5
     if (i % 5 == 0) {
       ctx.beginPath();
       ctx.moveTo(CENTER_X, CENTER_Y - 134);
       ctx.lineTo(CENTER_X, CENTER_Y - 118);
       ctx.stroke();
-      ctx.fillText(i, CENTER_X, CENTER_Y - 95);
+      ctx.fillText(i, CENTER_X, 105);
     } // end if
+    
+    // else draw a short mark
     else {
       ctx.beginPath();
       ctx.moveTo(CENTER_X, CENTER_Y - 134);
       ctx.lineTo(CENTER_X, CENTER_Y - 124);
       ctx.stroke();
     }
-    ctx.translate(CENTER_X, CENTER_Y);  
+    
+    // rotate after drawing each mark
+    ctx.translate(CENTER_X, CENTER_Y);
     ctx.rotate((2 * Math.PI) / 40);
     ctx.translate(-CENTER_X, -CENTER_Y);
   }
+  
+  // draw the logo
   ctx.font = "bolder 22px serif";
   ctx.textBaseline = "middle";
   ctx.fillText("Dogo", CENTER_X, CENTER_Y);
 } // end drawNumbers
+
+/***************************************************************/
+
+// This function turns the lock's dial one time
+
+function turn(direction) {
+  ctx.save();
+  ctx.resetTransform();
+  ctx.clearRect(0, 0, 500, 400);
+  drawLock();
+  ctx.restore();
+  
+  ctx.translate(CENTER_X, CENTER_Y);
+  if (direction == "right") {
+    ctx.rotate((2 * Math.PI) / 40);
+    number = (number + 40 - 1) % 40;
+  }
+  else if (direction == "left") {
+    ctx.rotate( -(2 * Math.PI) / 40);
+    number = (number + 1) % 40;
+  }
+  ctx.translate(-CENTER_X, -CENTER_Y);
+  drawNumbers();
+  
+  // when called from the toTarget function this code interrupts
+  // the timer when the targeted number is achieved
+  if (number == target) {
+    clearInterval(timer);
+  }
+  console.log(number);
+} // end turn
+
+/***************************************************************/
+
+// This function turns the lock's dial to target number
+
+function toTarget(direction, form) {
+  target = form.elements["numField"].value;
+  timer = window.setInterval(turn, 150, direction);
+} // end toTarget
